87a41719795041cf2761efb323b77dfe3ddbcd4a
Roy <royamunoz@gmail.com>
2017-10-07 14:35:52 -0600

    Initial commit

---
+++ Scrum-Manager/Scrum-Manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Scrum_Manager.Startup))]
+namespace Scrum_Manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
