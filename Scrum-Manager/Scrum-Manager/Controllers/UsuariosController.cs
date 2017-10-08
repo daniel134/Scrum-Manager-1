1f6b7d3c5f407001304c98d103dc39d9d109b62e
Roy <royamunoz@gmail.com>
2017-10-07 19:15:52 -0600

    -Cambio de estilo a la pagina -Se agrega el Controllers/UsuariosController.cs y sus vistas

---
+++ Scrum-Manager/Scrum-Manager/Controllers/UsuariosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Scrum_Manager.Models;
+
+namespace Scrum_Manager.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private equipojorgeEntities db = new equipojorgeEntities();
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            var usuario = db.Usuario.Include(u => u.AspNetRoles).Include(u => u.Proyecto1);
+            return View(usuario.ToList());
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.rol_FK = new SelectList(db.AspNetRoles, "Id", "Name");
+            ViewBag.proyecto_FK = new SelectList(db.Proyecto, "id_proyecto", "nombre");
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nombre,apellido1,apellido2,cedula,sexo,email,proyecto_FK,rol_FK")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.rol_FK = new SelectList(db.AspNetRoles, "Id", "Name", usuario.rol_FK);
+            ViewBag.proyecto_FK = new SelectList(db.Proyecto, "id_proyecto", "nombre", usuario.proyecto_FK);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.rol_FK = new SelectList(db.AspNetRoles, "Id", "Name", usuario.rol_FK);
+            ViewBag.proyecto_FK = new SelectList(db.Proyecto, "id_proyecto", "nombre", usuario.proyecto_FK);
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "nombre,apellido1,apellido2,cedula,sexo,email,proyecto_FK,rol_FK")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.rol_FK = new SelectList(db.AspNetRoles, "Id", "Name", usuario.rol_FK);
+            ViewBag.proyecto_FK = new SelectList(db.Proyecto, "id_proyecto", "nombre", usuario.proyecto_FK);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            db.Usuario.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
